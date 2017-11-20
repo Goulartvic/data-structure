bcc537139e033440d8ce2f4074f3076f4bcd327b
victor.goulart <victor.goulart@neoway.com.br>
2017-11-20 13:10:31 -0200

    Model created

---
+++ src/model/Player.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Player {
+
+    private String[] name;
+    private double[] salary;
+    private String[] team;
+
+    public String[] getName() {
+        return name;
+    }
+
+    public void setName(String[] name) {
+        this.name = name;
+    }
+
+    public double[] getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double[] salary) {
+        this.salary = salary;
+    }
+
+    public String[] getTeam() {
+        return team;
+    }
+
+    public void setTeam(String[] team) {
+        this.team = team;
+    }
+}
