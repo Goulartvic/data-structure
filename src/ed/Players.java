46dab5bb9bdd0eb33386305ad779ce03d8be4c32
victor.goulart <victor.goulart@neoway.com.br>
2017-11-23 19:19:37 -0200

    Deleted switch case and add simple filter by Name, Team or Salary

--- src/ed/Players.java
+++ src/ed/Players.java
@@ -7,9 +7,9 @@ public class Players {
 
     private Map<Integer, String> names = new TreeMap<>();
     private Map<Integer, String> teams = new TreeMap<>();
-    private Map<Integer, Integer> salary = new TreeMap<>();
+    private Map<Integer, String> salary = new TreeMap<>();
 
-    public Players(Map<Integer, String> names, Map<Integer, String> teams, Map<Integer, Integer> salary) {
+    public Players(Map<Integer, String> names, Map<Integer, String> teams, Map<Integer, String> salary) {
         this.names = names;
         this.teams = teams;
         this.salary = salary;
@@ -22,5 +22,17 @@ public class Players {
                 ", teams=" + teams +
                 ", salary=" + salary +
                 '}';
+    }
+
+    public Map<Integer, String> getNames() {
+        return names;
+    }
+
+    public Map<Integer, String> getTeams() {
+        return teams;
+    }
+
+    public Map<Integer, String> getSalary() {
+        return salary;
     }
 }
