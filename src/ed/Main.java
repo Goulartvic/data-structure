e2da1c63a13e7411e8ce9938dd947d0dd3e414ee
victor.goulart <victor.goulart@neoway.com.br>
2017-11-23 15:10:39 -0200

    Salary now has fixed values

--- src/ed/Main.java
+++ src/ed/Main.java
@@ -1,4 +1,14 @@
 package ed;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+
+        controller.mapPreview();
+
+        controller.insertPlayers();
+
+        System.out.println(controller.printMap());
+    }
 }
